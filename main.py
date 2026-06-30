c14721fbd53974126141056d46c99e127326defd
Ankit Gupta <2523ankitgupta@gmail.com>
2026-06-30 16:50:54 +0530

    Added movement to Player block and added clock tick to 60

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import player
 pygame.init
 screen=pygame.display.set_mode((800,800))
 running=True
+clock=pygame.time.Clock()
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+    screen.fill((0,0,0))
     player.draw(screen)
+    player.movement()
     pygame.display.update()
+    clock.tick(60)
     
 pygame.quit()
--- player.py
+++ player.py
@@ -1,4 +1,28 @@
 import pygame
 
+x=400
+y=730
+speed=5
+width=45
+
+def movement():
+    global x,y
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        x+=speed
+    if keys[pygame.K_LEFT]:
+        x-=speed
+    if keys[pygame.K_UP]:
+        y-=speed
+    if keys[pygame.K_DOWN]:
+        y+=speed
+    if x>=800-width:
+        x=800-width
+    if x<=width:
+        x=width
+    if y>=800-width:
+        y=800-width
+    if y<width:
+        y=width
 def draw(screen):
-    pygame.draw.rect(screen,(255,0,255),(400,730,45,45))
+    pygame.draw.rect(screen,(255,0,255),(x,y,width,width))
