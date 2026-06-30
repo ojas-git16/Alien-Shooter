69dcee71c4de3e5fdd2d805c87c9c10e45084fe0
ojas-git16 <ojas162006@gmail.com>
2026-06-30 18:47:52 +0530

    Add alien spawning and movement

---
+++ alien.py
@@ -0,0 +1,48 @@
+import pygame
+import random
+
+width = 45
+height = 45
+speed = 2
+
+aliens=[]       # there might be multiple aliens on the screen at same time
+
+spawn_timer = 0
+spawn_delay = 120   # clock is set to 120. alien spawns every alternate second
+
+game_over = False
+
+def spawn():
+    global spawn_timer
+    global spawn_delay
+    global speed
+    spawn_timer += 1
+    if (spawn_timer%600==0 and spawn_delay >= 80):   # increases alien spawn speed with time
+        spawn_delay-=10
+    if (spawn_timer == 1800):
+        speed+=1
+    if (spawn_timer%spawn_delay)==0:
+        x = random.randint(0, 800-width)
+        aliens.append({"x" : x , "y" : -height})  # y=-height because it appears above the window
+
+def movement():
+    global aliens
+    global game_over
+    for alien in aliens:
+        alien["y"]+=speed
+
+    # for alien in aliens:
+    #     if (alien["y"] >= 800):
+    #         game_over = True
+    
+    # in case game down not end, if alien reaches bottom so updating the aliens list, so it does not overflow
+    new_aliens = []
+    for alien in aliens:
+        if(alien["y"]<800):
+            new_aliens.append(alien)
+    aliens = new_aliens
+
+def draw(screen):
+    for alien in aliens:
+        pygame.draw.rect(screen, (0, 255, 0), (alien["x"], alien["y"], width, height))
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+import alien
 import player
 
-pygame.init
+pygame.init()
 screen=pygame.display.set_mode((800,800))
 running=True
 clock=pygame.time.Clock()
@@ -9,9 +10,15 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+
+    if not alien.game_over:
+        alien.spawn()
+        alien.movement()
+        player.movement()
+    
     screen.fill((0,0,0))
     player.draw(screen)
-    player.movement()
+    alien.draw(screen)
     pygame.display.update()
     clock.tick(60)
     
