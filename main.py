d42d92808aaf4b568595238bd4a614fed8d6340f
Ankit Gupta <2523ankitgupta@gmail.com>
2026-06-30 16:31:47 +0530

    added player block

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
+import player
 
 pygame.init
-screen=pygame.display.set_mode((800,600))
+screen=pygame.display.set_mode((800,800))
 running=True
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+    player.draw(screen)
+    pygame.display.update()
     
 pygame.quit()
---
+++ player.py
@@ -0,0 +1,4 @@
+import pygame
+
+def draw(screen):
+    pygame.draw.rect(screen,(255,0,255),(400,730,45,45))
